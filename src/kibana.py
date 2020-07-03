e8e56394ea6b4040c8999ad78b765e95f367ec28
SayanNL1996 <sayan.mandal@nineleaps.com>
2020-07-03 20:39:24 +0530

    added comments on function and readme file

--- src/kibana.py
+++ src/kibana.py
@@ -1,6 +1,7 @@
 import logging
 import random
 from src import connect as c
+
 """
 This is POC which is created to understand how kibana analyse logs.  
 """
@@ -8,12 +9,11 @@ logging.basicConfig(filename="logFilecovid.txt",
                     filemode='a',
                     format='%(asctime)s %(levelname)s-%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-for i in range(0,15):
-    x=random.randint(0,2)
-    if(x==0):
+for i in range(0, 15):
+    x = random.randint(0, 2)
+    if (x == 0):
         logging.warning('Log Message')
-    elif(x==1):
+    elif (x == 1):
         logging.critical('Log Message')
     else:
         logging.error('Log Message')
-
